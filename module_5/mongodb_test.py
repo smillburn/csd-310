71264bbefb597c954afb2d4e36070fa4f3c5b4fe
PREDATOR\millb <millburndragon@gmail.com>
2021-06-20 10:58:52 -0500

    Module 5.2

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.gs5ga.mongodb.net/pytech"
+
+client = MongoClient(url)
+db = client.pytech
+
+print(db.list_collection_names())
+
+
+
