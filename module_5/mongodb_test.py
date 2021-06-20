6c9c61556699f8e6789f34a88529639e0e0769ef
PREDATOR\millb <millburndragon@gmail.com>
2021-06-20 11:29:34 -0500

    Add insert file Module 5.3

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,3 +1,6 @@
+# Assignee: Skyler Millburn
+# Assignment: 5.2
+# Description: Test connection to MongoDB
 from pymongo import MongoClient
 
 url = "mongodb+srv://admin:admin@cluster0.gs5ga.mongodb.net/pytech"
---
+++ module_5/pytech_insert.py
@@ -0,0 +1,39 @@
+# Assignee: Skyler Millburn
+# Assignment: 5.3
+# Description: insert data into student collection
+
+from pymongo import MongoClient
+
+
+fred = {
+	"student_id": "1007",
+	"first_name": "Fred",
+    "last_name": "Bob",
+}
+bert = {
+	"student_id": "1008",
+	"first_name": "Bert",
+    "last_name": "Buffalo",
+    
+}
+martha = {
+	"student_id": "1009",
+	"first_name": "Martha",
+    "last_name": "Ruha",	
+}
+
+url = "mongodb+srv://admin:admin@cluster0.gs5ga.mongodb.net/pytech"
+
+client = MongoClient(url)
+db = client.pytech
+col = db.students
+
+print("-- Insert Statements --")
+id1 = col.insert_one(fred)
+print(f"Inserted Student Record Fred Bob into the students collection with document_id {id1.inserted_id}")
+
+id2 = col.insert_one(bert)
+print(f"Inserted Student Record Bert Buffalo into the students collection with document_id {id2.inserted_id}")
+
+id3 = col.insert_one(martha)
+print(f"Inserted Student Record Martha Ruha into the students collection with document_id {id3.inserted_id}")
